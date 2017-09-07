c9afd108da28269c93d65d83c63e0984246413e6
Sustavov Anton <sustav86@gmail.com>
2017-09-07 19:48:43 +0300

    HW2_6

---
+++ src/main/java/ru/javawebinar/topjava/repository/mock/InMemoryUserRepositoryImpl.java
@@ -0,0 +1,57 @@
+package ru.javawebinar.topjava.repository.mock;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Repository;
+import ru.javawebinar.topjava.model.Role;
+import ru.javawebinar.topjava.model.User;
+import ru.javawebinar.topjava.repository.UserRepository;
+
+import java.util.*;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.function.Consumer;
+
+@Repository
+public class InMemoryUserRepositoryImpl implements UserRepository {
+    private static final Logger log = LoggerFactory.getLogger(InMemoryUserRepositoryImpl.class);
+    private Map<Integer, User> repository = new ConcurrentHashMap<>();
+    private AtomicInteger counter = new AtomicInteger(0);
+
+    public static final int USER_ID = 1;
+    public static final int ADMIN_ID = 0;
+
+    {
+        save(new User(1, "Admin", "admin@gm.com", "1", Role.ROLE_ADMIN));
+        save(new User(2, "User", "user@gm.com", "2", Role.ROLE_USER));
+    }
+    @Override
+    public boolean delete(int id) {
+        return repository.remove(id) != null;
+    }
+
+    @Override
+    public User save(User user) {
+        if (user.isNew()) {
+            user.setId(counter.incrementAndGet());
+        }
+        return repository.put(user.getId(), user);
+    }
+
+    @Override
+    public User get(int id) {
+        return repository.get(id);
+    }
+
+    @Override
+    public Collection<User> getAll() {
+        return repository.values();
+    }
+
+    @Override
+    public User getByEmail(String email) {
+        Optional<User> optional = repository.entrySet().stream().filter(u -> u.getValue().getEmail().equals(email)).findFirst().map(Map.Entry::getValue);
+//        return getAll().stream().filter(u -> u.getEmail().equals(email)).findFirst().orElse(null);
+        return optional.orElse(null);
+    }
+}
