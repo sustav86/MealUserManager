3d01c4dd00d912b0a7afbcec9a0f28759d0909db
Sustavov Anton <sustav86@gmail.com>
2017-09-09 18:57:44 +0300

    W10_4

--- src/main/webapp/resources/js/datatablesUtil.js
+++ src/main/webapp/resources/js/datatablesUtil.js
@@ -24,16 +24,10 @@ function add() {
 function updateRow(id) {
     $.get(ajaxUrl + id, function (data) {
         $.each(data, function (key, value) {
-            form.find("input[name='" + key + "']").val(
-                key === "dateTime" ? formatDate(value) : value
-            );
+            form.find("input[name='" + key + "']").val(value);
         });
         $('#editRow').modal();
     });
-}
-
-function formatDate(date) {
-    return date.replace('T', ' ').substr(0, 16);
 }
 
 function deleteRow(id) {
